2c05531cf8fbea52110c230beee9438e9f0d9022
Ocean_Moist <Rohanzealous@Gmail.com>
2021-09-08 20:34:57 -0700

    Update MyMain.java

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MyMain {
 
     // Calculates the factorial of a number
-    public static double factorial(double x) {
+    public static double factorial(int x) {
         int fact = 1;
         for (int i = x; i >= 1; i--) {
             fact *= i;
